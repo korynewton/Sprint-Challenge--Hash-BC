89b437b02058124d8f76017df4920a76510c2147
Kory Newton <korybnewton@gmail.com>
2019-07-12 10:36:49 -0700

    completes ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,16 +3,30 @@ from hashtables import (HashTable,
                         hash_table_insert,
                         hash_table_remove,
                         hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_resize,
+                        )
+
+
+# input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+# output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
 
+    for i in range(len(weights)):
+        # number we will be looking for in hashtable to complete a valid pair
+        valid_pair = limit - weights[i]
+
+        # searcch hash table for the value that would work
+        does_exist = hash_table_retrieve(ht, valid_pair)
+
+        if does_exist:
+            print(does_exist, i)
+            return (does_exist, i)
     return None
 
 
@@ -21,3 +35,6 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21)
